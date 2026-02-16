80f7919c012db24c98d8cae4b85fd7f511a359c0
Sebastian <sebastian.kosciolek2000@gmail.com>
2026-02-16 12:32:26 +0100

    update

--- lib/api.ts
+++ lib/api.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const LOCAL_IP = '192.168.0.16'; 
-const API_URL = `http://${LOCAL_IP}:8000/api`;
+const LOCAL_IP = '192.168.18.6'; 
+const PORT = '8000';
+const API_URL = `http://${LOCAL_IP}:${PORT}/api`;
 
 const api = axios.create({
     baseURL: API_URL,
